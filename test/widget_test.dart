cbf063f738a6c7ac58fc18a2bfee1314937008cc
lepapillon90 <lepapillon00@naver.com>
2026-09-13 10:50:10 +0900

    feat: add office pixel assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,5 @@
 import 'package:ai_office/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -6,5 +7,17 @@ void main() {
     await tester.pumpWidget(const OfficeApp());
 
     expect(find.text('AI Office'), findsOneWidget);
+  });
+
+  testWidgets('loads the player sprite asset', (tester) async {
+    await tester.pumpWidget(const OfficeApp());
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Image(image: AssetImage('assets/images/characters/office_worker.png')),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(tester.takeException(), isNull);
   });
 }
